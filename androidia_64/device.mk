e3eb562834a6e58093d3927c7816afe60c9aab1e
Kalyan Kondapally <kalyan.kondapally@intel.com>
2016-11-26 02:47:47 -0800

    Include Media configurations.
    Bug ID: None.
    Test: Able to play video in browser.
    
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- androidia_64/device.mk
+++ androidia_64/device.mk
@@ -318,6 +318,23 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.opengles.version=196609
 ##############################################################
+# Source: device/intel/mixins/groups/media/android_ia/product.mk
+##############################################################
+# Audio/video codec support.
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    device/intel/android_ia/common/media/media_profiles.xml:system/etc/media_profiles.xml \
+    device/intel/android_ia/common/media/media_codecs.xml:system/etc/media_codecs.xml
+
+#Enable deep buffer for video playback
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.audio.deep=true
+
+# Enable AAC 5.1 output
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.aac_51_output_enabled=true
+##############################################################
 # Source: device/intel/mixins/groups/device-type/tablet/product.mk
 ##############################################################
 PRODUCT_CHARACTERISTICS := tablet
