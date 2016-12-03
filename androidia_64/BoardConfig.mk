b5ae5f2dfb4e3e4100648a591ba910da583cda32
Kalyan Kondapally <kalyan.kondapally@intel.com>
2016-12-03 06:38:15 -0800

    Update to latest Mixin changes.
    Includes following changes:
    1) Modules are now installed to /system/lib/modules instead of
    /system/lib/modules/<KernelRelease>/
    2)Firmware is installed to /system/etc/lib
    
    Bug: None.
    Test: Boots to Home screen.
    
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- androidia_64/BoardConfig.mk
+++ androidia_64/BoardConfig.mk
@@ -30,6 +30,9 @@ TARGET_2ND_ARCH_VARIANT := x86
 
 BOARD_USE_64BIT_USERSPACE := true
 TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_KERNEL_ARCH ?= x86_64
+BOARD_USE_64BIT_KERNEL ?= true
+TARGET_USES_64_BIT_BINDER := true
 
 # customize the malloced address to be 16-byte aligned
 BOARD_MALLOC_ALIGNMENT := 16
@@ -42,6 +45,8 @@ WITH_DEXPREOPT_PIC := true
 endif
 
 # the following variables could be overridden
+TARGET_PRELINK_MODULE := false
+TARGET_NO_KERNEL ?= false
 TARGET_NO_RECOVERY ?= true
 TARGET_HAS_THIRD_PARTY_APPS := true
 
@@ -64,6 +69,8 @@ KERNELFLINGER_IGNORE_RSCI := true
 KERNELFLINGER_SSL_LIBRARY := boringssl
 # Specify system verity partition
 #PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/by-name/system
+
+BOARD_KERNEL_CMDLINE += root=/dev/ram0 androidboot.hardware=$(TARGET_PRODUCT) androidboot.selinux=permissive
 
 # Use the non-open-source parts, if they're present
 -include vendor/BoardConfigVendor.mk
@@ -163,7 +170,7 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_PRELINK_MODULE := false
 TARGET_NO_KERNEL ?= false
 
-BOARD_KERNEL_CMDLINE += root=/dev/ram0 androidboot.hardware=$(TARGET_PRODUCT) androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += root=/dev/ram0 androidboot.hardware=$(TARGET_PRODUCT) androidboot.selinux=permissive firmware_class.path=/system/etc/firmware
 ##############################################################
 # Source: device/intel/mixins/groups/bluetooth/btusb/BoardConfig.mk
 ##############################################################
@@ -171,4 +178,5 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_LINUX := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/intel/common/bluetooth/bcm43241/
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-bt-pan
+
 # ------------------ END MIX-IN DEFINITIONS ------------------
