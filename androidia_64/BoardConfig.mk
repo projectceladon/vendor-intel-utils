2609da7f51167fa7c8bb3b5235280c4ef099e7f2
Randy Xu <randy.xu@intel.com>
2016-12-23 11:35:41 +0800

    Solve the ABI list regression in board configure
    This regression is caused by recent Mixin patch set, the TARGET_CPU_ABI was not defined when set TARGET_CPU_ABI_LIST. 
    Then the x86_64,x86 will be missed in ro.product.cpu.abilist, and causes the x86/x86_64 ABI app installation failure.
    
    JIRA: https://01.org/jira/browse/AIA-74
    Test: Can install the Android CTS for x86/x86_64 ABI

--- androidia_64/BoardConfig.mk
+++ androidia_64/BoardConfig.mk
@@ -57,27 +57,6 @@ WITH_NATIVE_BRIDGE := true
 # Enable ARM codegen for x86 with Native Bridge
 BUILD_ARM_FOR_X86 := true
 
-# Native Bridge ABI List
-NB_ABI_LIST_32_BIT := armeabi-v7a armeabi
-NB_ABI_LIST_64_BIT := arm64-v8a
-
-# Support 64 Bit Apps
-ifeq ($(ENABLE_NATIVEBRIDGE_64BIT),true)
-  TARGET_CPU_ABI_LIST_64_BIT ?= $(TARGET_CPU_ABI) $(TARGET_CPU_ABI2)
-  TARGET_CPU_ABI_LIST_32_BIT ?= $(TARGET_2ND_CPU_ABI) $(TARGET_2ND_CPU_ABI2)
-  TARGET_CPU_ABI_LIST := \
-      $(TARGET_CPU_ABI_LIST_64_BIT) \
-      $(TARGET_CPU_ABI_LIST_32_BIT) \
-      $(NB_ABI_LIST_32_BIT) \
-      $(NB_ABI_LIST_64_BIT)
-  TARGET_CPU_ABI_LIST_32_BIT += $(NB_ABI_LIST_32_BIT)
-  TARGET_CPU_ABI_LIST_64_BIT += $(NB_ABI_LIST_64_BIT)
-else
-  TARGET_CPU_ABI_LIST_32_BIT ?= $(TARGET_CPU_ABI) $(TARGET_CPU_ABI2)
-  TARGET_CPU_ABI_LIST_32_BIT += $(NB_ABI_LIST_32_BIT)
-  TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_32_BIT)
-endif
-
 BOARD_SEPOLICY_M4DEFS += module_houdini=true
 BOARD_SEPOLICY_DIRS += device/intel/android_ia/sepolicy/houdini
 ##############################################################
@@ -166,6 +145,28 @@ else
 TARGET_ARCH := x86
 TARGET_CPU_ABI := x86
 endif
+
+# Native Bridge ABI List
+NB_ABI_LIST_32_BIT := armeabi-v7a armeabi
+NB_ABI_LIST_64_BIT := arm64-v8a
+
+# Support 64 Bit Apps
+ifeq ($(ENABLE_NATIVEBRIDGE_64BIT),true)
+  TARGET_CPU_ABI_LIST_64_BIT ?= $(TARGET_CPU_ABI) $(TARGET_CPU_ABI2)
+  TARGET_CPU_ABI_LIST_32_BIT ?= $(TARGET_2ND_CPU_ABI) $(TARGET_2ND_CPU_ABI2)
+  TARGET_CPU_ABI_LIST := \
+      $(TARGET_CPU_ABI_LIST_64_BIT) \
+      $(TARGET_CPU_ABI_LIST_32_BIT) \
+      $(NB_ABI_LIST_32_BIT) \
+      $(NB_ABI_LIST_64_BIT)
+  TARGET_CPU_ABI_LIST_32_BIT += $(NB_ABI_LIST_32_BIT)
+  TARGET_CPU_ABI_LIST_64_BIT += $(NB_ABI_LIST_64_BIT)
+else
+  TARGET_CPU_ABI_LIST_32_BIT ?= $(TARGET_CPU_ABI) $(TARGET_CPU_ABI2)
+  TARGET_CPU_ABI_LIST_32_BIT += $(NB_ABI_LIST_32_BIT)
+  TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_32_BIT)
+endif
+
 ##############################################################
 # Source: device/intel/mixins/groups/rfkill/true/BoardConfig.mk
 ##############################################################
