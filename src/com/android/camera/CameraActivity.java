2d6d4d23a96d011f6d6927b6e244529773256157
Jeff Sharkey <jsharkey@android.com>
2017-06-20 16:39:16 -0600

    Only onboard when launched through front door.
    When an app is using public API to take a photo, don't confuse the
    user with the onboarding flow.
    
    Test: cts-tradefed run commandAndExit cts-dev -m CtsProviderTestCases -t android.provider.cts.MediaStoreUiTest#testImageCapture
    Bug: 62204676
    Change-Id: Ie0de7b879302a5dd4bf941bf31b449bda95cfdf7

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -1862,7 +1862,7 @@ public class CameraActivity extends QuickActivity
             finish();
             return;
         }
-        if (!mSecureCamera) {
+        if (!isSecureCamera() && !isCaptureIntent()) {
             // Show the dialog if necessary. The rest resume logic will be invoked
             // at the onFirstRunStateReady() callback.
             try {
