6954fb6589d2704b73b3d7db56afcf986616fbcd
buildslave <ctbbot@intel.com>
2018-07-16 05:52:22 +0000

    mixin updates
    Add cel_kbl_acrn target
    mixins.spec is based on internal ww27 acrn version.
    device-specific: add cel_kbl_acrn option
    copy from ww27 gordon_peak_acrn.
    
    Change-Id: Icafb036b9a2d19ad78607d17fa01ad5010f51d02
    Tracked-On: OAM-66570
    Tracked-On:OAM-66570
    Mixin-Reviewed-On: #/c/637949
    Mixin-Reviewed-On: #/c/638233
    Signed-off-by: Chen Lin Z <lin.z.chen@intel.com>

---
+++ cel_kbl_acrn/extra_files/acrn-guest/md5_check.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+check_sum_imgs=(flash_AaaG.json sos_boot.img sos_rootfs.img partition_desc.bin)
+
+if [ -a "$1/md5sum.md5" ];then
+	cat $1/md5sum.md5 | while read line
+	do
+		var_name=`echo $line | awk '{print $2}'`
+
+		for img in ${check_sum_imgs[@]}; do
+			if [ "$img" = "$var_name" ]; then
+				md5_val=`echo $line | awk '{print $1}'`
+				md5_img=`md5sum $1/$img | awk '{print $1}'`
+
+				count=1
+				while [ $count -le 20 ];
+				do
+					if [ "$md5_img" != "$md5_val" ]; then
+						echo "file $img download is continue ......"
+						sleep 60
+						count=$((count + 1))
+						md5_img=`md5sum $1/$img | awk '{print $1}'`
+					else
+						break
+					fi
+				done
+
+				if [ $count -gt 20 ];then
+					echo "network exception, download $img failed!"
+					exit 1
+				else
+					echo "$img: md5 check successfully!"
+				fi
+			fi
+		done
+	done
+else
+	echo "ERROR: md5sum.md5 file NOT FOUND!"
+fi
---
+++ cel_kbl_acrn/extra_files/trusty/trusty_vsbl.mk
@@ -0,0 +1,42 @@
+LK_ELF := $(TRUSTY_BUILDROOT)build-sand-x86-64/lk.elf
+
+SIGNING_KEY := $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).pk8
+SIGNING_CERT := $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).x509.pem
+
+INSTALLED_TOS_IMAGE_TARGET := $(PRODUCT_OUT)/tos.img
+
+tosimage: $(INSTALLED_TOS_IMAGE_TARGET)
+
+ifeq ($(wildcard $(TOS_PREBUILT)), )
+ifeq (true,$(BOARD_AVB_ENABLE)) # BOARD_AVB_ENABLE == true
+$(INSTALLED_TOS_IMAGE_TARGET): lk $(MKBOOTIMG) $(AVBTOOL)
+	@echo "mkbootimg to create boot image for TOS file: $@"
+	$(hide) $(MKBOOTIMG) --kernel $(LK_ELF) --output $@
+	@echo "$(AVBTOOL): add hashfooter to TOS file: $@"
+	$(hide) $(AVBTOOL) add_hash_footer \
+		--image $@ \
+		--partition_size $(BOARD_TOSIMAGE_PARTITION_SIZE) \
+		--partition_name tos $(INTERNAL_AVB_SIGNING_ARGS)
+INSTALLED_VBMETAIMAGE_TARGET ?= $(PRODUCT_OUT)/vbmeta.img
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --include_descriptors_from_image $(INSTALLED_TOS_IMAGE_TARGET)
+$(INSTALLED_VBMETAIMAGE_TARGET): $(INSTALLED_TOS_IMAGE_TARGET)
+else
+$(INSTALLED_TOS_IMAGE_TARGET): lk $(MKBOOTIMG) $(BOOT_SIGNER)
+	@echo "mkbootimg to create boot image for TOS file: $@"
+	$(hide) $(MKBOOTIMG) --kernel $(LK_ELF) --output $@
+	$(if $(filter true,$(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_SUPPORTS_BOOT_SIGNER)),\
+		@echo "$(BOOT_SIGNER): sign prebuilt TOS file: $@" &&\
+		$(BOOT_SIGNER) /tos $@ $(SIGNING_KEY) $(SIGNING_CERT) $@)
+endif
+else
+$(INSTALLED_TOS_IMAGE_TARGET):
+	@echo "Use prebuilt tos image $(TOS_PREBUILT)"
+	$(hide) (cp $(TOS_PREBUILT) $(INSTALLED_TOS_IMAGE_TARGET))
+
+ifeq (true,$(BOARD_AVB_ENABLE)) # BOARD_AVB_ENABLE == true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --include_descriptors_from_image $(INSTALLED_TOS_IMAGE_TARGET)
+endif
+
+endif
+
+INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_TOS_IMAGE_TARGET)
