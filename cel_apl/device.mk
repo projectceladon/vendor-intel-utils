4a4341c5982e583c8f922cc5c4911a66d4c392e7
buildslave <ctbbot@intel.com>
2018-08-09 07:32:46 +0000

    mixin updates
    mixins.spec: Specify USB config for gordonpeak
    USB config should be adb for Gordonpeak  platform.
    usb-gadget : make usb_config configurable
    On GP-ACRN usb_config is not changing from mtp to adb
    in time.
    For now make it configurable so that it does not take the
    original mtp value.
    
    Change-Id: I498982ff0fee211e162d8b58c1df900d48826641
    Tracked-On: OAM-66778
    Mixin-Reviewed-On: #/c/630884
    Mixin-Reviewed-On: #/c/640340
    Signed-off-by: Abhilash K V <abhilash.k.v@intel.com>
    Signed-off-by: M Balaji <m.balaji@intel.com>
    Signed-off-by: saranya <saranya.gopal@intel.com>

--- cel_apl/device.mk
+++ cel_apl/device.mk
@@ -446,7 +446,9 @@ ifeq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
 ifeq ($(BUILD_FOR_CTS_AUTOMATION),true)
 # Build for automated CTS
+ifneq ($(USB_CONFIG), adb)
 USB_CONFIG := $(USB_CONFIG),adb
+endif
 PRODUCT_COPY_FILES += $(INTEL_PATH_COMMON)/usb-gadget/adb_keys:root/adb_keys
 endif #BUILD_FOR_CTS_AUTOMATION == true
 endif #TARGET_BUILD_VARIANT == user
