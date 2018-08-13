3091b2c6237e3d194c690fab7186c853c3f2c475
buildslave <ctbbot@intel.com>
2018-08-13 07:42:05 +0000

    mixin updates
    [Thermal]: switch the usb OTG tool to CBC protocol
    The SLCAN protocol was replaced by CBC currently.
    This patch adjusts the tool with CBC protocol accordingly.
    
    Change-Id: Icef7ef28244a93f70910c686679a0216528fd131
    Tracked-On: OAM-67299
    Mixin-Reviewed-On: #/c/640065
    Signed-off-by: Wang Chaox <chaox.m.wang@intel.com>

--- mixins/groups/usb-otg-switch/true/usb_otg_switch.sh
+++ mixins/groups/usb-otg-switch/true/usb_otg_switch.sh
@@ -2,26 +2,16 @@
 
 KERNEL_VERSION=$(uname -r)
 
-if [ $(which cansend) ]; then
-  CANSEND=$(which cansend)
-elif [ -e /sbin/cansend ]; then
-  CANSEND=/sbin/cansend
-elif [ -e /vendor/bin/cansend ]; then
-  CANSEND=/vendor/bin/cansend
-else
-  echo "Cansend not found!"
-fi
-
 echo $1
 if [[ $1 = "h" ]]; then
-  $CANSEND slcan0 "0000FFFF#0C010155555555"
+  echo -n -e "\x0a\x01\x01" > /dev/cbc-diagnosis
   case "$KERNEL_VERSION" in
     4.4*) echo h > /sys/bus/platform/devices/intel-cht-otg.0/mux_state ;;
     4.9*) echo h > /sys/bus/platform/drivers/intel_usb_dr_phy/intel_usb_dr_phy.0/mux_state ;;
        *) echo "host" > /sys/class/usb_role/intel_xhci_usb_sw-role-switch/role ;;
   esac
 elif [[ $1 = "p" ]]; then
-  $CANSEND slcan0 "0000FFFF#0C010055555555"
+  echo -n -e "\x0a\x01\x00" > /dev/cbc-diagnosis
   case "$KERNEL_VERSION" in
     4.4*) echo p > /sys/bus/platform/devices/intel-cht-otg.0/mux_state ;;
     4.9*) echo p > /sys/bus/platform/drivers/intel_usb_dr_phy/intel_usb_dr_phy.0/mux_state ;;
