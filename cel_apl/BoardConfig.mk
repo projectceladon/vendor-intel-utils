fce2235be18ad31db07041068275face2263632a
buildslave <ctbbot@intel.com>
2018-09-06 06:47:57 +0000

    mixin updates
    Enable the updater_esp, which is used for OTA when disable slot-ab.
    
    Change-Id: Iad30ef7995985ca4d626a1317eec145959b3383c
    Tracked-On: OAM-68195
    Mixin-Reviewed-On: #/c/643561
    Signed-off-by: Ming Tan <ming.tan@intel.com>

--- cel_apl/BoardConfig.mk
+++ cel_apl/BoardConfig.mk
@@ -35,10 +35,13 @@ TARGET_RELEASETOOLS_EXTENSIONS ?= $(INTEL_PATH_HARDWARE)/bootctrl/recovery
 
 # Adds edify commands swap_entries and copy_partition for robust
 # update of the EFI system partition
-#TARGET_RECOVERY_UPDATER_LIBS := libupdater_esp
+TARGET_RECOVERY_UPDATER_LIBS := libupdater_esp
 # Extra libraries needed to be rolled into recovery updater
 # libgpt_static and libefivar are needed by libupdater_esp
-#TARGET_RECOVERY_UPDATER_EXTRA_LIBS := libcommon_recovery libgpt_static libefivar
+TARGET_RECOVERY_UPDATER_EXTRA_LIBS := libcommon_recovery libgpt_static
+ifeq ($(TARGET_SUPPORT_BOOT_OPTION),true)
+TARGET_RECOVERY_UPDATER_EXTRA_LIBS += libefivar
+endif
 
 # By default recovery minui expects RGBA framebuffer
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
--- mixins/groups/boot-arch/efi/BoardConfig.mk
+++ mixins/groups/boot-arch/efi/BoardConfig.mk
@@ -9,12 +9,17 @@ TARGET_RECOVERY_FSTAB ?= $(TARGET_DEVICE_DIR)/fstab.recovery
 # to the OTA updater scripts
 TARGET_RELEASETOOLS_EXTENSIONS ?= $(INTEL_PATH_HARDWARE)/bootctrl/recovery
 
+{{^slot-ab}}
 # Adds edify commands swap_entries and copy_partition for robust
 # update of the EFI system partition
-#TARGET_RECOVERY_UPDATER_LIBS := libupdater_esp
+TARGET_RECOVERY_UPDATER_LIBS := libupdater_esp
 # Extra libraries needed to be rolled into recovery updater
 # libgpt_static and libefivar are needed by libupdater_esp
-#TARGET_RECOVERY_UPDATER_EXTRA_LIBS := libcommon_recovery libgpt_static libefivar
+TARGET_RECOVERY_UPDATER_EXTRA_LIBS := libcommon_recovery libgpt_static
+ifeq ($(TARGET_SUPPORT_BOOT_OPTION),true)
+TARGET_RECOVERY_UPDATER_EXTRA_LIBS += libefivar
+endif
+{{/slot-ab}}
 
 # By default recovery minui expects RGBA framebuffer
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
