0aaea54f09dcccd939e88cc5722c6799417a34bc
buildslave <ctbbot@intel.com>
2018-10-15 17:32:59 +0000

    mixin updates
    mixins.spec: Specify USB config for gordonpeak
    USB config default should be adb for broxton platform.
    Defining carservice_app as module for selinux policy
    sepolicy module only is needed to differentiate bxt
    platforms like gordonpeak and leafhill.
    broxton platform supports gordonpeak and leafhill targets .
    Gordonpeak is mainly for IVI hence car app is availble in gordon peak
    only.sys.usb.cfg permission needed only in car app. Targets like
    leafhill which do not need car service app dont provide permission hence
    moduleonly introduced for sys.usb.cfg for car app permission.
    
    Change-Id: I0df60be2c5630d823659ffb447b451e19e908359
    Tracked-On: OAM-69907
    Mixin-Reviewed-On: #/c/647801
    Mixin-Reviewed-On: #/c/648714
    Signed-off-by: M Balaji <m.balaji@intel.com>

--- cel_kbl_acrn/BoardConfig.mk
+++ cel_kbl_acrn/BoardConfig.mk
@@ -314,6 +314,7 @@ BOARD_SEPOLICY_DIRS += \
     $(INTEL_PATH_SEPOLICY)/car
 
 TARGET_USES_CAR_FUTURE_FEATURES := true
+BOARD_SEPOLICY_M4DEFS += module_carservice_app=true
 ##############################################################
 # Source: device/intel/project-celadon/mixins/groups/gms/true/BoardConfig.mk
 ##############################################################
--- mixins/groups/device-type/car/BoardConfig.mk
+++ mixins/groups/device-type/car/BoardConfig.mk
@@ -4,3 +4,4 @@ BOARD_SEPOLICY_DIRS += \
     $(INTEL_PATH_SEPOLICY)/car
 
 TARGET_USES_CAR_FUTURE_FEATURES := true
+BOARD_SEPOLICY_M4DEFS += module_carservice_app=true
