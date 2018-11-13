c80100d4e28a2a08b8ce7acde6d1952d9d0ffb9f
buildslave <ctbbot@intel.com>
2018-11-13 11:35:45 +0000

    mixin updates
    use ro.vendor.ifwi_version to check version
    edit fw_update to use ro.vendor.ifwi_version to check ifwi version
    
    Change-Id: Id95dadee7109b688ddbe19074d16eed58893f565
    Tracked-On: OAM-71519
    Mixin-Reviewed-On: #/c/651568
    Signed-off-by: Xihua Chen <xihua.chen@intel.com>

--- mixins/groups/slot-ab/true/update_ifwi_boot.sh
+++ mixins/groups/slot-ab/true/update_ifwi_boot.sh
@@ -6,7 +6,6 @@ IFWIFAIL=0
 CMDIFWIFAIL=$(cat /proc/cmdline | grep "^.*ABL.ifwifail=" | sed 's/^.*ABL.ifwifail=//' | cut -d " " -f1 | cut -d "," -f2)
 CMDIFWIVER1=$(cat /proc/cmdline | grep "^.*ABL.bpdt1=" | sed 's/^.*ABL.bpdt1=//' | cut -d " " -f1 | cut -d "," -f2)
 CMDIFWIVER2=$(cat /proc/cmdline | grep "^.*ABL.bpdt2=" | sed 's/^.*ABL.bpdt2=//' | cut -d " " -f1 | cut -d "," -f2)
-FILEIFWIVER=/mnt/ifwi.version
 
 if [ "$CMDIFWIFAIL" == "07" ]; then
     echo "IFWI ERR: IFWI_UPDATE_ERR_CSE"
@@ -39,23 +38,15 @@ if [ "$IFWIFAIL" != "0" ]; then
     fi
 fi
 
-if [ "$SLOT_SUFFIX" = "_a" ]; then
-    /vendor/bin/toybox_vendor dd if=/dev/block/by-name/bootloader_a of=$FILEIFWIVER bs=1024 skip=7990 count=1
-    RAWIFWIVER_A=$(cat $FILEIFWIVER | head -n 10)
-    rm $FILEIFWIVER
-elif [ "$SLOT_SUFFIX" = "_b" ]; then
-    /vendor/bin/toybox_vendor dd if=/dev/block/by-name/bootloader_b of=$FILEIFWIVER bs=1024 skip=7990 count=1
-    RAWIFWIVER_B=$(cat $FILEIFWIVER | head -n 10)
-    rm $FILEIFWIVER
-fi
+RAWIFWIVER=$(getprop ro.vendor.ifwi_version)
 if [ "$CMDIFWIVER1" != "" ] && [ "$CMDIFWIVER2" != "" ]; then
     if [ "$CMDIFWIVER1" = "$CMDIFWIVER2" ]; then
         if [ "$SLOT_SUFFIX" = "_a" ]; then
-            if [ "$RAWIFWIVER_A" != "$CMDIFWIVER1" ] && [ "$RAWIFWIVER_A" != "" ]; then
+            if [ "$RAWIFWIVER" != "$CMDIFWIVER1" ] && [ "$RAWIFWIVER" != "" ]; then
                 setprop vendor.ota.update.fw a
             fi
         elif [ "$SLOT_SUFFIX" = "_b" ]; then
-            if [ "$RAWIFWIVER_B" != "$CMDIFWIVER1" ] && [ "$RAWIFWIVER_B" != "" ]; then
+            if [ "$RAWIFWIVER" != "$CMDIFWIVER1" ] && [ "$RAWIFWIVER" != "" ]; then
                 setprop vendor.ota.update.fw b
             fi
         fi
