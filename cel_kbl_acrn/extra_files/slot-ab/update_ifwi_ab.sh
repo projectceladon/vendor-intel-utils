57240b3522b3e7c51e4229f72b47ef0b947bfbe0
buildslave <ctbbot@intel.com>
2018-11-16 04:03:19 +0000

    mixin updates
    support update ioc in postinstall
    Currently, OTA update need to reboot twice for IOC update,
    Optimize it to just reboot once for updating Android P.
    
    Change-Id: Ifdcb3633a7ba26040b415eec5ee931fb635aaee3
    Tracked-On: OAM-71655
    Mixin-Reviewed-On: #/c/651286
    Signed-off-by: Xihua Chen <xihua.chen@intel.com>

--- cel_kbl_acrn/extra_files/slot-ab/update_ifwi_ab.sh
+++ cel_kbl_acrn/extra_files/slot-ab/update_ifwi_ab.sh
@@ -8,4 +8,12 @@ elif [ "$SLOT_SUFFIX" = "_b" ]; then
     setprop vendor.ota.update.fw a
 fi
 
+IOC_FLASH_TOOL=/vendor/bin/ioc_flash_server
+
+if [ -f ${IOC_FLASH_TOOL} ]; then
+    cp -r /postinstall/firmware/ioc /mnt
+    setprop vendor.ioc.update run
+    sleep 30
+fi
+
 exit 0
