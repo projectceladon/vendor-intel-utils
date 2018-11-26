b4f6576fba8bbd46e5fe7b20986d72762c23a7fb
buildslave <ctbbot@intel.com>
2018-11-26 06:50:33 +0000

    mixin updates
    init.rc: Remove reference to init.svc.adb
    init.svc.adb is not an exported platform
    property. So, remove its reference from
    init.rc.
    configfs: Remove reference of 'persist.sys.usb.config'
    persist.sys.usb.config is not an exported property.
    So, remove its reference from product makefile.
    
    Change-Id: I32675af6a70ea8b364054219667dd2fce1464614
    Tracked-On: OAM-71792
    Tracked-On: OAM-71836
    Mixin-Reviewed-On: #/c/652441
    Mixin-Reviewed-On: #/c/652443
    Signed-off-by: saranya <saranya.gopal@intel.com>

--- cel_apl/device.mk
+++ cel_apl/device.mk
@@ -490,26 +490,14 @@ PRODUCT_PACKAGES += \
 ##############################################################
 # Source: device/intel/project-celadon/mixins/groups/usb-gadget/configfs/product.mk
 ##############################################################
-# Set default USB interface
-USB_CONFIG := mtp
-
 ifeq ($(TARGET_BUILD_VARIANT),user)
 # Enable Secure Debugging
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
 
-ifeq ($(USB_CONFIG), adb)
-USB_CONFIG := mtp
-endif
-
 ifeq ($(BUILD_FOR_CTS_AUTOMATION),true)
-# Build for automated CTS
-ifneq ($(USB_CONFIG), adb)
-USB_CONFIG := $(USB_CONFIG),adb
-endif
 PRODUCT_COPY_FILES += $(INTEL_PATH_COMMON)/usb-gadget/adb_keys:root/adb_keys
 endif #BUILD_FOR_CTS_AUTOMATION == true
 endif #TARGET_BUILD_VARIANT == user
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=$(USB_CONFIG)
 
 # Add Intel adb keys for userdebug/eng builds
 ifneq ($(TARGET_BUILD_VARIANT),user)
