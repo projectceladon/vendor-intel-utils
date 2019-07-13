9a71e91101b4825600be6f65e8391004650d6903
Felix <google@ix5.org>
2019-07-13 15:55:03 +0200

    Rework permissions request flow
    Don't ask for location permissions if the user hasn't first opted into
    saving geolocation.
    
    Also add some TODOs for converting PermissionsActivity into a dialog and
    for Android Q's NFC deprecation
    
    Signed-off-by: Felix <google@ix5.org>
    Change-Id: Iab927e3202ea1b70e2cfd1b8671e3c900d2f2eb6

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -192,9 +192,6 @@ public class CameraActivity extends QuickActivity
     private static final long SCREEN_DELAY_MS = 2 * 60 * 1000; // 2 mins.
     /** Load metadata for 10 items ahead of our current. */
     private static final int FILMSTRIP_PRELOAD_AHEAD_ITEMS = 10;
-    private static final int PERMISSIONS_ACTIVITY_REQUEST_CODE = 1;
-    private static final int PERMISSIONS_RESULT_CODE_OK = 1;
-    private static final int PERMISSIONS_RESULT_CODE_FAILED = 2;
 
     /** Should be used wherever a context is needed. */
     private Context mAppContext;
@@ -848,6 +845,7 @@ public class CameraActivity extends QuickActivity
                 messageId > 0 ? getString(messageId) : "");
     }
 
+    // Candidate for deletion as Android Beam is deprecated in Android Q
     private void setupNfcBeamPush() {
         NfcAdapter adapter = NfcAdapter.getDefaultAdapter(mAppContext);
         if (adapter == null) {
@@ -1631,6 +1629,7 @@ public class CameraActivity extends QuickActivity
 
         preloadFilmstripItems();
 
+        // Candidate for deletion as Android Beam is deprecated in Android Q
         setupNfcBeamPush();
 
         mLocalImagesObserver = new FilmstripContentObserver();
@@ -1903,10 +1902,14 @@ public class CameraActivity extends QuickActivity
         } else {
             mHasCriticalPermissions = false;
         }
-
-        if ((checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                !mSettingsManager.getBoolean(SettingsManager.SCOPE_GLOBAL, Keys.KEY_HAS_SEEN_PERMISSIONS_DIALOGS)) ||
-                !mHasCriticalPermissions) {
+        if (!mHasCriticalPermissions || (mSettingsManager.getBoolean(
+                SettingsManager.SCOPE_GLOBAL, Keys.KEY_RECORD_LOCATION) &&
+                (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                   != PackageManager.PERMISSION_GRANTED) &&
+                !mSettingsManager.getBoolean(SettingsManager.SCOPE_GLOBAL,
+                    Keys.KEY_HAS_SEEN_PERMISSIONS_DIALOGS))) {
+            // TODO: Convert PermissionsActivity into a dialog so we
+            // don't lose the state of CameraActivity.
             Intent intent = new Intent(this, PermissionsActivity.class);
             startActivity(intent);
             finish();
