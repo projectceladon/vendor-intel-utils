2adbeb8ae694a70aee41dbf40863bf1d172ed645
yjsun1 <yi.j.sun@intel.com>
2019-07-22 10:09:23 +0800

    modify autopatch.sh & vendorsetup.sh script
    Signed-off-by: yjsun1 <yi.j.sun@intel.com>

--- autopatch.sh
+++ autopatch.sh
@@ -15,10 +15,16 @@
 # more details.
 
 top_dir=`pwd`
+if [ ! "${top_dir}" ]; then
+    echo "[autopatch.sh] Couldn't locate the top of the tree.  Try setting TOP." >&2
+    return
+fi
 utils_dir="$top_dir/vendor/intel/utils"
-patch_dir="$utils_dir/android_p/google_diff/$TARGET_PRODUCT"
-private_utils_dir="$top_dir/vendor/intel/PRIVATE/utils"
-private_patch_dir="$private_utils_dir/android_p/google_diff/$TARGET_PRODUCT"
+patch_dir_aosp="$utils_dir/aosp_diff"
+patch_dir_bsp="$utils_dir/bsp_diff"
+private_utils_dir="$top_dir/vendor/intel/utils_priv"
+private_patch_dir_aosp="$private_utils_dir/aosp_diff"
+private_patch_dir_bsp="$private_utils_dir/bsp_diff"
 
 current_project=""
 previous_project=""
@@ -39,7 +45,6 @@ apply_patch() {
     current_project=`dirname $i`
     if [[ $current_project != $previous_project ]]; then
       echo ""
-      echo ""
       echo "Project $current_project"
     fi
     previous_project=$current_project
@@ -58,39 +63,70 @@ apply_patch() {
     fi
 
     cd $top_dir/$current_project
-    a=`grep "Date: " $pd/$i`
+    a=`grep "Date: " ${pd}/$i`
     b=`echo ${a#"Date: "}`
     c=`git log --pretty=format:%aD | grep "$b"`
 
     if [[ "$c" == "" ]] ; then
-      git am $pd/$i >& /dev/null
+      git am -3 --keep-cr --whitespace=nowarn $pd/$i >& /dev/null
       if [[ $? == 0 ]]; then
-        echo "        Applying          $i"
+        echo "        Applying          `basename $i`"
       else
-        echo "        Conflicts          $i"
+        echo "        Conflicts          `basename $i`"
         git am --abort >& /dev/null
         conflict="y"
         conflict_list="$current_project $conflict_list"
       fi
     else
-      echo "        Already applied         $i"
+      echo "        Already applied         `basename $i`"
       applied_already="y"
     fi
   done
 }
 
-#Apply common patches
-cd $patch_dir
-patch_list=`find * -iname "*.patch" | sort -u`
-apply_patch "$patch_list" "$patch_dir"
+function fpnat() # find patch files and apply them
+{
+    local patch_top_dir=$1
+    # arg #1 should be the .patch files' top directory,
+    # either aosp_diff/common or aosp_diff/${TARGET_PRODUCT}
+    # and bsp_diff/common bsp_diff/${TARGET_PRODUCT} directories.
+    cd ${patch_top_dir}
+    patch_list=`find * -iname "*.patch" | sort -u`
+    apply_patch "${patch_list}" "${patch_top_dir}"
+    if [[ $? != 0 ]]; then
+        echo "Apply ${patch_top_dir} patches failure!"
+    fi
+    unset patch_list patch_top_dir
+}
 
-#Apply Embargoed patches if exist
-if (( 0 )) ; then
-    echo ""
-    echo "Embargoed Patches Found"
-    cd $private_patch_dir
-    private_patch_list=`find * -iname "*.patch" | sort -u`
-    apply_patch "$private_patch_list" "$private_patch_dir"
+echo "Apply utils/aosp_diff/common patches:"
+fpnat "$patch_dir_aosp/common"
+echo "apply utils/aosp_diff/common done, sleep 60"
+sleep 60
+patch_list=`find * -iname "*.patch" | sort -u`
+apply_patch "$patch_list" "${patch_dir_aosp}/common"
+unset patch_list
+
+if [[ -e ${patch_dir_aosp}/${TARGET_PRODUCT} ]] && [[ -d ${patch_dir_aosp}/${TARGET_PRODUCT} ]];then
+        echo "Apply utils/aosp_diff Target ${TARGET_PRODUCT} Patches:"
+        cd ${patch_dir_aosp}/${TARGET_PRODUCT}
+        patch_list=`find * -iname "*.patch" | sort -u`
+        apply_patch "$patch_list" "${patch_dir_aosp}/${TARGET_PRODUCT}"
+        unset patch_list
+        if [[ $? != 0 ]]; then
+            echo "Apply ${patch_dir_aosp}/${TARGET_PRODUCT} patches failure!"
+        fi
+fi
+
+if [[ -e ${patch_dir_bsp}/${TARGET_PRODUCT} ]] && [[ -d ${patch_dir_bsp}/${TARGET_PRODUCT} ]];then
+        echo "Apply utils/bsp_diff Target ${TARGET_PRODUCT} Patches:"
+        cd ${patch_dir_bsp}/${TARGET_PRODUCT}
+        patch_list=`find * -iname "*.patch" | sort -u`
+        apply_patch "$patch_list" "${patch_dir_bsp}/${TARGET_PRODUCT}"
+        unset patch_list
+        if [[ $? != 0 ]]; then
+            echo "Apply ${patch_dir_bsp}/${TARGET_PRODUCT} patches failure!"
+        fi
 fi
 
 echo ""
@@ -104,12 +140,77 @@ if [[ "$conflict" == "y" ]]; then
   echo '$(error "Conflicts seen while applying lunch patches !! Resolve and re-trigger")' > $top_dir/vendor/intel/utils/Android.mk
 else
   echo "==========================================================================="
-  echo "           SUCCESS : All patches applied fine !!                           "
+  echo -e "SUCCESS : All patches applied fine in: \n$patch_dir_aosp \nand \n${patch_dir_bsp} !!"
   echo "==========================================================================="
   if [[ "$applied_already" == "y" ]]; then
     echo "==========================================================================="
     echo "           INFO : SOME PATCHES ARE APPLIED ALREADY  !!                     "
     echo "==========================================================================="
   fi
-  sed -i '/^/d' $top_dir/vendor/intel/utils/Android.mk
+  if [ -f "$top_dir/vendor/intel/utils/Android.mk" ]; then
+          sed -i '/^/d' $top_dir/vendor/intel/utils/Android.mk
+  fi
+fi
+
+echo ""
+echo "Apply Embargoed patches if exist"
+if [[ -e ${private_utils_dir} ]] && [[ -d ${private_utils_dir} ]];then
+        echo "Embargoed Patches Found"
+
+    ## TMEP solution, please remove this part ##
+    if [[ -e ${private_patch_dir_aosp}/common ]] && [[ -d ${private_patch_dir_aosp}/common ]];then
+        cd ${private_patch_dir_aosp}/common
+        patch_list=`find * -iname "*.patch" | sort -u`
+        apply_patch "$patch_list" "${private_patch_dir_aosp}/common"
+        unset patch_list
+        if [[ $? != 0 ]]; then
+            echo "Apply ${private_patch_dir_aosp}/common patches failure!"
+        fi
+    fi
+    ## TMEP solution, please remove this part ##
+
+    if [[ -e ${private_patch_dir_aosp}/${TARGET_PRODUCT} ]] && [[ -d ${private_patch_dir_aosp}/${TARGET_PRODUCT} ]];then
+        echo "Apply utils_priv/aosp_diff Target ${TARGET_PRODUCT} Patches:"
+        cd ${private_patch_dir_aosp}/${TARGET_PRODUCT}
+        patch_list=`find * -iname "*.patch" | sort -u`
+        apply_patch "${patch_list}" "$private_patch_dir_aosp/${TARGET_PRODUCT}"
+        unset patch_list
+        if [[ $? != 0 ]]; then
+            echo "Apply ${private_patch_dir_aosp}/${TARGET_PRODUCT} patches failure!"
+        fi
+    fi
+
+    if [[ -e ${private_patch_dir_bsp}/${TARGET_PRODUCT} ]] && [[ -d ${private_patch_dir_bsp}/${TARGET_PRODUCT} ]];then
+        echo "Apply utils_priv/bsp_diff Target ${TARGET_PRODUCT} Patches:"
+        cd ${private_patch_dir_bsp}/${TARGET_PRODUCT}
+        patch_list=`find * -iname "*.patch" | sort -u`
+        apply_patch "${patch_list}" "${private_patch_dir_bsp}/${TARGET_PRODUCT}"
+        unset patch_list
+        if [[ $? != 0 ]]; then
+            echo "Apply ${private_patch_dir_bsp}/${TARGET_PRODUCT} patches failure!"
+        fi
+    fi
+fi
+
+echo ""
+if [[ "$conflict" == "y" ]]; then
+  echo "==========================================================================="
+  echo "           ALERT : Conflicts Observed while patch application !!           "
+  echo "==========================================================================="
+  for i in $conflict_list ; do echo $i; done | sort -u
+  echo "==========================================================================="
+  echo -e "Error: Please resolve Conflict(s) and re-run lunch..."
+  echo '$(error "Conflicts seen while applying lunch patches !! Resolve and re-trigger")' > $top_dir/vendor/intel/utils_priv/Android.mk
+else
+  echo "==========================================================================="
+  echo -e "           SUCCESS : All patches applied fine in \n${private_patch_dir_aosp} \nand \n${private_patch_dir_bsp}"
+  echo "==========================================================================="
+  if [[ "$applied_already" == "y" ]]; then
+    echo "==========================================================================="
+    echo "           INFO : SOME PATCHES ARE APPLIED ALREADY  !!                     "
+    echo "==========================================================================="
+  fi
+  if [ -f "$top_dir/vendor/intel/utils_priv/Android.mk" ]; then
+          sed -i '/^/d' $top_dir/vendor/intel/utils_priv/Android.mk
+  fi
 fi
---
+++ vendorsetup.sh
@@ -0,0 +1,92 @@
+# Get the AOSP Ring on OneAndroid
+# Should be a comment in the manifest in the form
+# <!-- Celadon_Ring='1' -->
+
+# save the official lunch command to aosp_lunch() and source it
+tmp_lunch=`mktemp`
+sed '/ lunch()/,/^}/!d'  build/envsetup.sh | sed 's/function lunch/function aosp_lunch/' > ${tmp_lunch}
+source ${tmp_lunch}
+rm -f ${tmp_lunch}
+
+function  apply_patch
+{
+
+local patch_folder=$1
+echo "patch folder: $patch_folder"
+# Check if there is a list of files to parse and apply patches listed in them if any
+for file in `find $patch_folder -type f -o -type l 2>/dev/null` ; do
+    if [[ "$TARGET_PRODUCT" == $(basename $file) ]]; then
+        echo "Applying patche(s) needed for $TARGET_PRODUCT"
+        vendor/intel/utils_priv/autopatch.py -f $file --remote=origin
+        local ret=$?
+        if [ $ret -ne 0 ]; then
+            local files_with_issues="$files_with_issues $file"
+        fi
+        # If some patch does not apply create Android.mk to stop compilation.
+        if [ -n "$files_with_issues" ]; then
+            echo "\$(error \"[GOOGLE_DIFF] Some patches given to autopatch did not applied correctly in $files_with_issues.\") " > vendor/intel/utils/Android.mk
+        fi
+    fi
+done
+
+}
+
+# Override lunch function to filter lunch targets
+function lunch
+{
+    local T=$(gettop)
+    if [ ! "$T" ]; then
+        echo "[lunch] Couldn't locate the top of the tree.  Try setting TOP." >&2
+        return
+    fi
+
+    aosp_lunch $*
+
+    rm -rf vendor/intel/utils/Android.mk vendor/intel/utils_priv/Android.mk
+    vendor/intel/utils/autopatch.sh
+
+    local aosp_patch_folder=vendor/intel/utils_priv/google_diff
+    local bsp_patch_folder=vendor/intel/utils_priv/bsp_diff
+    for file in $aosp_patch_folder $bsp_patch_folder; do
+            apply_patch $file
+    done
+
+    # check if product configuration files are out of date
+    if [[ $(find device/intel -path "*mixins*" -prune -o -name ${TARGET_PRODUCT}.mk -print) ]]; then
+        product_dir=$(dirname $(find device/intel -path "*mixins*" -prune -o -name ${TARGET_PRODUCT}.mk -print))
+        mixinup -s $product_dir/mixins.spec -d
+    fi
+}
+
+# Get the exact value of a build variable.
+function get_build_var()
+{
+    if [ "$1" = "COMMON_LUNCH_CHOICES" ]
+    then
+        valid_targets=`mixinup -t`
+        save=`build/soong/soong_ui.bash --dumpvar-mode $1`
+        unset LUNCH_MENU_CHOICES
+        for t in ${save[@]}; do
+            array=(${t/-/ })
+            target=${array[0]}
+            if [[ "${valid_targets}" =~ "$target" ]]; then
+                   LUNCH_MENU_CHOICES+=($t)
+            fi
+        done
+        echo ${LUNCH_MENU_CHOICES[@]}
+        return
+    else
+        if [ "$BUILD_VAR_CACHE_READY" = "true" ]
+        then
+            eval "echo \"\${var_cache_$1}\""
+            return
+        fi
+
+        local T=$(gettop)
+        if [ ! "$T" ]; then
+            echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
+            return
+        fi
+        (\cd $T; build/soong/soong_ui.bash --dumpvar-mode $1)
+    fi
+}
