82316948edd2495e6bf6059b183fb0f84668fb64
buildslave <ctbbot@intel.com>
2019-08-29 08:43:49 +0000

    mixin updates
    Change mixin config for using HWC 2.3 API.
    Compile & link HWC 2.3 and integrate HWC 2.3 service.
    [CEL] Modify Audio Configurations
    Update celadon audio configuration based on hw capabilities,
    remove unused configs.
    [PATCH] Add AIC launch target
    
    Change-Id: I33ad1772f44ccb88ebbcaa5ed42328cc228684e9
    Tracked-On: OAM-84818
    Tracked-On: OAM-84902
    Tracked-On: OAM-85369
    Mixin-Reviewed-On: #/c/677801
    Mixin-Reviewed-On: #/c/678899
    Mixin-Reviewed-On: #/c/678933
    Signed-off-by: Karan Patidar <karan.patidar@intel.com>
    Signed-off-by: Wan Shuang <shuang.wan@intel.com>
    Signed-off-by: Wang, Liang <liang.wang@intel.com>

--- celadon_ivi/device.mk
+++ celadon_ivi/device.mk
@@ -205,15 +205,32 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service
 
+#Audio policy engine configuration files
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/default/effect/audio_effects.xml:vendor/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/default/policy/audio_policy_criteria.conf:vendor/etc/audio_policy_criteria.conf \
-    $(LOCAL_PATH)/audio/default/policy/audio_policy_configuration.xml:vendor/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/default/mixer_paths_0.xml:vendor/etc/mixer_paths_0.xml \
     $(LOCAL_PATH)/audio/default/policy/audio_policy_engine_product_strategies.xml:vendor/etc/audio_policy_engine_product_strategies.xml \
     $(LOCAL_PATH)/audio/default/policy/audio_policy_engine_configuration.xml:vendor/etc/audio_policy_engine_configuration.xml \
     $(LOCAL_PATH)/audio/default/policy/audio_policy_engine_criteria.xml:vendor/etc/audio_policy_engine_criteria.xml \
-    $(LOCAL_PATH)/audio/default/policy/audio_policy_engine_criterion_types.xml.in:vendor/etc/audio_policy_engine_criterion_types.xml.in \
+    $(LOCAL_PATH)/audio/default/policy/audio_policy_engine_criterion_types.xml.in:vendor/etc/audio_policy_engine_criterion_types.xml.in
+
+# Vendor audio configuration files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/default/policy/audio_policy_configuration.xml:vendor/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/default/policy/a2dp_audio_policy_configuration.xml:vendor/etc/a2dp_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/default/policy/r_submix_audio_policy_configuration.xml:vendor/etc/r_submix_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/default/policy/usb_audio_policy_configuration.xml:vendor/etc/usb_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/default/policy/audio_policy_volumes.xml:vendor/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/default/policy/default_volume_tables.xml:vendor/etc/default_volume_tables.xml \
+    $(LOCAL_PATH)/audio/default/effect/audio_effects.xml:vendor/etc/audio_effects.xml \
+    $(LOCAL_PATH)/audio/default/mixer_paths_0.xml:vendor/etc/mixer_paths_0.xml
+
+#fallback
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/default/policy/fallback/audio_policy_configuration_generic.xml:system/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/default/policy/fallback/primary_audio_policy_configuration.xml:system/etc/primary_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/default/policy/fallback/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/default/policy/fallback/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/default/policy/fallback/default_volume_tables.xml:system/etc/default_volume_tables.xml
 
 
 PRODUCT_PROPERTY_OVERRIDES += audio.safemedia.bypass=true
@@ -274,8 +291,7 @@ PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl \
                     android.hardware.graphics.allocator@2.0-impl \
                     android.hardware.graphics.allocator@2.0-service \
                     android.hardware.renderscript@1.0-impl \
-                    android.hardware.graphics.composer@2.1-impl \
-                    android.hardware.graphics.composer@2.1-service \
+                    android.hardware.graphics.composer@2.3-service \
                     libbt-vendor
 
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/manifest.xml:vendor/manifest.xml
--- celadon_tablet/audio/AndroidBoard.mk
+++ celadon_tablet/audio/AndroidBoard.mk
@@ -2,108 +2,20 @@ LOCAL_PATH := $(call my-dir)
 
 # audio hardware is not discoverable, select hardware or use basic default
 ifeq ($(INTEL_AUDIO_HAL), stub)
-
-# this option only enables USB, A2DP and remote submix audio.
-# the audio.primary.default HAL is part of the build to pass
-# CTS tests but only acts as a NULL sink/source
-
 AUDIO_HARDWARE := stub
-
 else
 AUDIO_HARDWARE := default
-#AUDIO_HARDWARE := PCH-CX20724
-# Next configuration is used for Intel NUC6i5SYH
-#AUDIO_HARDWARE := PCH-ALC283
-#AUDIO_HARDWARE := nuc-skull-canyon
 endif
-
-###########################################
-# Audio stack Packages
-###########################################
-include $(CLEAR_VARS)
-LOCAL_MODULE := audio_configuration_files
-LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := \
-    audio_policy_configuration_files \
-    audio_settings_configuration_files
 
 ifeq ($(INTEL_AUDIO_HAL), stub)
 LOCAL_REQUIRED_MODULES += audio.stub.default
 else
 LOCAL_REQUIRED_MODULES += audio.primary.$(TARGET_BOARD_PLATFORM)
-
-ifeq ($(INTEL_AUDIO_HAL),audio_pfw)
-LOCAL_REQUIRED_MODULES += audio_hal_configuration_files
 endif
-
-endif
-
-include $(BUILD_PHONY_PACKAGE)
-
-###########################################
-# Audio Policy Configuration files
-###########################################
-include $(CLEAR_VARS)
-LOCAL_MODULE := audio_policy_configuration_files
-LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := \
-    a2dp_audio_policy_configuration.xml \
-    r_submix_audio_policy_configuration.xml \
-    usb_audio_policy_configuration.xml \
-    audio_policy_volumes.xml \
-    default_volume_tables.xml \
-    audio_policy_configuration.xml
 
 ifeq ($(INTEL_AUDIO_HAL), stub)
 LOCAL_REQUIRED_MODULES += stub_audio_policy_configuration.xml
 endif
-
-include $(BUILD_PHONY_PACKAGE)
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := a2dp_audio_policy_configuration.xml
-#LOCAL_MODULE_OWNER := intel
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-#LOCAL_SRC_FILES := default/policy/$(LOCAL_MODULE)
-#include $(BUILD_PREBUILT)
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := audio_policy_volumes.xml
-#LOCAL_MODULE_OWNER := intel
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-#LOCAL_SRC_FILES := default/policy/$(LOCAL_MODULE)
-#include $(BUILD_PREBUILT)
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := default_volume_tables.xml
-#LOCAL_MODULE_OWNER := intel
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-#LOCAL_SRC_FILES := default/policy/$(LOCAL_MODULE)
-#include $(BUILD_PREBUILT)
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := r_submix_audio_policy_configuration.xml
-#LOCAL_MODULE_OWNER := intel
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-#LOCAL_SRC_FILES := default/policy/$(LOCAL_MODULE)
-#include $(BUILD_PREBUILT)
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := usb_audio_policy_configuration.xml
-#LOCAL_MODULE_OWNER := intel
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-#LOCAL_SRC_FILES := default/policy/$(LOCAL_MODULE)
-#include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := stub_audio_policy_configuration.xml
@@ -113,76 +25,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
 LOCAL_SRC_FILES := default/policy/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := audio_policy_configuration.xml
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-LOCAL_SRC_FILES := $(AUDIO_HARDWARE)/policy/$(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-###########################################
-
-###########################################
-# Audio HAL configuration file
-###########################################
-include $(CLEAR_VARS)
-LOCAL_MODULE := audio_hal_configuration_files
-LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := \
-    vendor_audio_criteria.xml \
-    vendor_audio_criterion_types.xml
-
-include $(BUILD_PHONY_PACKAGE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := vendor_audio_criteria.xml
-LOCAL_MODULE_STEM := audio_criteria.xml
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-LOCAL_SRC_FILES := default/route/$(LOCAL_MODULE_STEM)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE :=  vendor_audio_criterion_types.xml
-LOCAL_MODULE_STEM :=  audio_criterion_types.xml
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-LOCAL_SRC_FILES := default/route/$(LOCAL_MODULE_STEM)
-include $(BUILD_PREBUILT)
-###########################################
-
-###########################################
-# Audio HAL Custom configuration files
-###########################################
-include $(CLEAR_VARS)
-LOCAL_MODULE := audio_settings_configuration_files
-LOCAL_MODULE_TAGS := optional
-ifeq ($(INTEL_AUDIO_HAL),audio_pfw)
-LOCAL_REQUIRED_MODULES := audio_parameter_framework
-else
-LOCAL_REQUIRED_MODULES := mixer_paths_0.xml
-endif
-include $(BUILD_PHONY_PACKAGE)
-
-ifeq ($(INTEL_AUDIO_HAL),audio_pfw)
-include audio/$(AUDIO_HARDWARE)/AndroidBoard.mk
-else
-include $(CLEAR_VARS)
-LOCAL_MODULE := mixer_paths_0.xml
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-LOCAL_SRC_FILES := $(AUDIO_HARDWARE)/$(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-endif
-###########################################
 
 ifeq ($(USE_CONFIGURABLE_AUDIO_POLICY), 1)
 include $(TARGET_DEVICE_DIR)/audio/reference_configurable_audio_policy/AndroidBoard.mk
