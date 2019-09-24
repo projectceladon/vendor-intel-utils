1b6f685d67f527ba1f9aa602526acbb881def2fd
buildslave <ctbbot@intel.com>
2019-09-24 18:42:33 +0000

    mixin updates
    Remove inclusion of incorrect camera service, impl and module
    Due to inclusion of incorrect camera service and libraries,
    error "Could not load camera HAL module" seen while running
    CtsMediaTestCases.
    
    Tracked-On: OAM-86606
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/397
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- celadon_tablet/device.mk
+++ celadon_tablet/device.mk
@@ -301,9 +301,6 @@ PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl \
                     android.hardware.usb@1.0-service \
                     android.hardware.dumpstate@1.0-impl \
                     android.hardware.dumpstate@1.0-service \
-                    camera.device@1.0-impl \
-                    android.hardware.camera.provider@2.4-impl \
-                    android.hardware.camera.provider@2.4-service \
                     android.hardware.graphics.mapper@2.0-impl \
                     android.hardware.graphics.allocator@2.0-impl \
                     android.hardware.graphics.allocator@2.0-service \
