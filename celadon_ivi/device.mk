37fd52d9aa0be103835d35a1ca104f3e99c6c464
buildslave <ctbbot@intel.com>
2019-09-25 13:13:52 +0000

    mixin updates
    i[CELADON]VtsHalGraphicsMapperV2_1Target module is skipped
    
    Tracked-On: OAM-85551
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/405
    Signed-off-by: swaroopb <swaroop.balan@intel.com>

--- celadon_ivi/device.mk
+++ celadon_ivi/device.mk
@@ -289,7 +289,7 @@ PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl \
                     android.hardware.usb@1.0-service \
                     android.hardware.dumpstate@1.0-impl \
                     android.hardware.dumpstate@1.0-service \
-                    android.hardware.graphics.mapper@2.0-impl \
+                     android.hardware.graphics.mapper@2.0-impl-2.1  \
                     android.hardware.graphics.allocator@2.0-impl \
                     android.hardware.graphics.allocator@2.0-service \
                     android.hardware.renderscript@1.0-impl \
