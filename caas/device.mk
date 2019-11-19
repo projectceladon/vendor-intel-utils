1af9dfdf2a2ea5b3e30d507753a4517d7ea029c3
buildslave <ctbbot@intel.com>
2019-11-19 17:09:54 +0000

    mixin updates
    Fix some treble violations
    There are some operations violate the treble requiements, and this
    introduces some selinux avc errors that cannot be fixed. Fix these
    violations so we can add proper sepolicy rules for this.
    
    Tracked-On: OAM-88545
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/444
    Signed-off-by: ji, zhenlong z <zhenlong.z.ji@intel.com>

--- caas/device.mk
+++ caas/device.mk
@@ -95,11 +95,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
 
+# move configure files provided by intel to vendor partition.
 PRODUCT_COPY_FILES += \
     $(INTEL_PATH_COMMON)/wlan/wpa_supplicant-common.conf:vendor/etc/wifi/wpa_supplicant.conf \
     $(INTEL_PATH_COMMON)/wlan/wpa_supplicant_overlay.conf:vendor/etc/wifi/wpa_supplicant_overlay.conf \
     $(INTEL_PATH_COMMON)/wlan/p2p_supplicant_overlay.conf:vendor/etc/wifi/p2p_supplicant_overlay.conf \
-    $(INTEL_PATH_COMMON)/wlan/wlan.conf:system/etc/modprobe.d/wlan.conf \
+    $(INTEL_PATH_COMMON)/wlan/wlan.conf:vendor/etc/modprobe.d/wlan.conf \
     frameworks/native/data/etc/android.hardware.wifi.xml:vendor/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:vendor/etc/permissions/android.hardware.wifi.direct.xml
 
@@ -364,8 +365,9 @@ PRODUCT_PACKAGES += ufo_prebuilts
 # i915 firmwares
 $(foreach fw,$(I915_FW),$(eval PRODUCT_PACKAGES += $(notdir $(fw))))
 
+# move configure files provided by intel to vendor partition
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/extra_files/graphics/drirc:system/etc/drirc
+    $(LOCAL_PATH)/extra_files/graphics/drirc:vendor/etc/drirc
 
 
 # HWComposer IA
