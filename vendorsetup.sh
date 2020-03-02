59089666b74aef3cde571365a79babb9a3709bdb
sgnanase <sundar.gnanasekaran@intel.com>
2020-03-02 21:49:36 +0530

    Migrate bsp/google diff patches to vendor/intel/utils
    Tracked-On: OAM-89816
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,26 +1,35 @@
+#
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This file is executed by build/envsetup.sh, and can use anything
+# defined in envsetup.sh.
+#
+# In particular, you can add lunch options with the add_lunch_combo
+# function: add_lunch_combo generic-eng
+
 # save the official lunch command to aosp_lunch() and source it
 tmp_lunch=`mktemp`
 sed '/ lunch()/,/^}/!d'  build/envsetup.sh | sed 's/function lunch/function aosp_lunch/' > ${tmp_lunch}
 source ${tmp_lunch}
 rm -f ${tmp_lunch}
 
-# Override lunch function to filter lunch targets
-function lunch
-{
-   aosp_lunch $*
-
-   rm -rf Android.mk
-   vendor/intel/utils/autopatch.sh
-
+function lunch {
+    aosp_lunch $*
+    vendor/intel/utils/android/apply_patch.sh
 }
 
-# Check that mixins are in sync with product configuration
-if [ -f device/intel/mixins/mixin-update ]; then
-    if ! device/intel/mixins/mixin-update --dry-run; then
-        echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
-        echo '+ Product configuration and mixins are out of sync                      +'
-        echo '+ PLEASE RE-RUN device/intel/mixins/mixin-update and commit the result! +'
-        echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
-    fi
-fi
-
+add_lunch_combo cic-userdebug
+add_lunch_combo cic_dev-userdebug
