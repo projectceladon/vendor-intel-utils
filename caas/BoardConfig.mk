f5adac15f31e3fd0b8cfd4606a6b1e0b92ac3033
zhezhu <zhe.zhu@intel.com>
2020-03-10 23:54:51 +0800

    Enable mediasdk_omx_il in Android Q caas target
    Signed-off-by: zhezhu <zhe.zhu@intel.com>
    Tracked-On: https://jira.devtools.intel.com/browse/OAM-90223

--- caas/BoardConfig.mk
+++ caas/BoardConfig.mk
@@ -365,6 +365,7 @@ INTEL_STAGEFRIGHT := true
 # Settings for the Media SDK library and plug-ins:
 # - USE_MEDIASDK: use Media SDK support or not
 USE_MEDIASDK := true
+BOARD_HAVE_OMX_SRC := true
 ##############################################################
 # Source: device/intel/mixins/groups/graphics/auto/BoardConfig.mk
 ##############################################################
