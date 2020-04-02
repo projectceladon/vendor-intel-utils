ad78594807d8a17cb79fecadd0290eba8e0131b0
buildslave <ctbbot@intel.com>
2020-04-02 04:20:39 +0000

    mixin updates
    Remove useless code in wifi module.
    
    Tracked-On: OAM-90566
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/574
    Signed-off-by: bxu10x <bingx.xu@intel.com>

--- cic/BoardConfig.mk
+++ cic/BoardConfig.mk
@@ -131,9 +131,7 @@ SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 ##############################################################
 # Source: device/intel/mixins/groups/wlan/mac80211_hwsim/BoardConfig.mk
 ##############################################################
-BOARD_WLAN_DEVICE           := emulator
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_simulated
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 WIFI_DRIVER_FW_PATH_PARAM   := "/dev/null"
 WIFI_DRIVER_FW_PATH_STA     := "/dev/null"
