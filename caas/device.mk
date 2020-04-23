14dfa3ba2c578ab9292ebd9ea89ac0938cd590eb
buildslave <ctbbot@intel.com>
2020-04-23 13:35:40 +0000

    mixin updates
    enable virtio-input device in CiV
    The change include:
    1. add more touchscreen config file
    2. enable virtio-input driver
    
    Tracked-On: OAM-90659
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/571
    Signed-off-by: Yang, Dong <dong.yang@intel.com>

--- caas/device.mk
+++ caas/device.mk
@@ -516,8 +516,9 @@ PRODUCT_COPY_FILES += \
 ##############################################################
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:vendor/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml\
-        $(INTEL_PATH_COMMON)/touch/Vendor_1ff7_Product_0f21.idc:system/usr/idc/Vendor_1ff7_Product_0f21.idc
-
+        $(INTEL_PATH_COMMON)/touch/Vendor_1ff7_Product_0f21.idc:system/usr/idc/Vendor_1ff7_Product_0f21.idc\
+        $(INTEL_PATH_COMMON)/touch/Vendor_2386_Product_3115.idc:system/usr/idc/Vendor_2386_Product_3115.idc\
+        $(INTEL_PATH_COMMON)/touch/Vendor_056a_Product_489c.idc:system/usr/idc/Vendor_056a_Product_489c.idc
 ##############################################################
 # Source: device/intel/mixins/groups/debug-tools/true/product.mk
 ##############################################################
