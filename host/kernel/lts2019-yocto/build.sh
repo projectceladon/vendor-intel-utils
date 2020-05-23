581312a2a801320b486d41df55dd4d59054b29e6
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2020-05-23 17:22:25 +0530

    yocto/host: host kernel patches, build scripts and README
    Changes include:
    - Added host kernel patches done on top of yocto kernel base.
    - Added repo tip build and weekly build script
    - Added README file
    
    Tracked-On: OAM-91113
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

---
+++ host/kernel/lts2019-yocto/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rm -rf host_kernel
+mkdir -p host_kernel
+cd host_kernel
+git clone https://github.com/projectceladon/vendor-intel-utils
+git clone https://github.com/intel/linux-intel-lts.git
+cd linux-intel-lts
+git checkout 61b917f571560989b82cf63142ee37c525e8ee5a
+cp ../vendor-intel-utils/host/kernel/lts2019-yocto/x86_64_defconfig .config
+patch_list=`find ../vendor-intel-utils/host/kernel/lts2019-yocto -iname "*.patch" | sort -u`
+for i in $patch_list
+do
+  a=`grep "Date: " $i`
+  b=`echo ${a#"Date: "}`
+  c=`git log --pretty=format:%aD | grep "$b"`
+  if [[ "$c" == "" ]] ; then
+    git am -3 --keep-cr --whitespace=nowarn $i >& /dev/null
+    if [[ $? == 0 ]]; then
+      echo -e "Patch\t"`basename $i` "\tApplied"
+    else
+      git am --abort
+      echo "Not able to apply\t"`basename $i`
+      break
+    fi
+  else
+    echo -e "\tAlready applied\t\t"`basename $i`
+  fi
+done
+make ARCH=x86_64 clean
+make ARCH=x86_64 olddefconfig
+make ARCH=x86_64 -j64 LOCALVERSION=-yvhb bindeb-pkg
