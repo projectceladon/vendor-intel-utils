d01f5105036fad502927d34c356ea8600854ecc6
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2020-05-26 17:58:58 +0530

    cic: Add kernel config and build script
    - Added kernel config, build scripts for chromium and
    yocto kernel bases.
    
    Tracked-On: OAM-91143
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

---
+++ host/lts2019-chromium/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+rm -rf cic_kernel
+mkdir -p cic_kernel
+cd cic_kernel
+git clone https://github.com/projectceladon/vendor-intel-utils -b celadon/p/mr0/cic
+git clone https://github.com/projectceladon/linux-intel-lts2019-chromium.git
+cd linux-intel-lts2019-chromium
+cp ../vendor-intel-utils/host/kernel/lts2019-chromium/x86_64_defconfig .config
+patch_list=`find ../vendor-intel-utils/host/kernel/lts2019-chromium -iname "*.patch" | sort -u`
+for i in $patch_list
+do
+  a=`grep "Date: " $i`
+  b=`echo ${a#"Date: "}`
+  c=`git log --pretty=format:%aD | grep "$b"`
+  if [[ "$c" == "" ]] ; then
+    git am -3 --keep-cr --whitespace=nowarn $i >& /dev/null
+    if [[ $? == 0 ]]; then
+      echo -e "Patch\t"`basename $i` "\tApplied"
+    else
+      git am --abort
+      echo "Not able to apply\t"`basename $i`
+      break
+    fi
+  else
+    echo -e "\tAlready applied\t\t"`basename $i`
+  fi
+done
+make ARCH=x86_64 clean
+make ARCH=x86_64 olddefconfig
+make ARCH=x86_64 -j64 LOCALVERSION=-cchb bindeb-pkg
