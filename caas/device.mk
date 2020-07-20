468f669d308eeb8f925f4d1866d5291706c9c334
buildslave <ctbbot@intel.com>
2020-07-20 14:52:46 +0000

    mixin updates
    Adding Accel, Gyro and Compass sensors permission.
    
    Tracked-On: OAM-91751
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/804
    Signed-off-by: Babu B, Ramesh <ramesh.babu.b@intel.com>

--- caas/device.mk
+++ caas/device.mk
@@ -736,7 +736,12 @@ PRODUCT_PACKAGES += \
         android.hardware.sensors@1.0-impl
 
 PRODUCT_COPY_FILES += \
-        frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:vendor/etc/permissions/android.hardware.sensor.ambient_temperature.xml
+        frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:vendor/etc/permissions/android.hardware.sensor.ambient_temperature.xml \
+        frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:vendor/etc/permissions/android.hardware.sensor.accelerometer.xml \
+        frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:vendor/etc/permissions/android.hardware.sensor.gyroscope.xml \
+        frameworks/native/data/etc/android.hardware.sensor.compass.xml:vendor/etc/permissions/android.hardware.sensor.compass.xml \
+        frameworks/native/data/etc/android.hardware.sensor.light.xml:vendor/etc/permissions/android.hardware.sensor.light.xml
+
 ##############################################################
 # Source: device/intel/mixins/groups/debug-unresponsive/default/product.mk
 ##############################################################
