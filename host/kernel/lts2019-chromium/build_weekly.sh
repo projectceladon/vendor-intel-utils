df3690ac4d55ca944cd973d7cbab1573b4d53854
Muhammad Aksar <muhammad.aksar@intel.com>
2020-08-19 12:50:45 +0530

    Chromium kernel rebase: Update commit hash and defconfig
    Tracked-On: OAM-92170
    Signed-off-by: Muhammad Aksar <muhammad.aksar@intel.com>

--- host/kernel/lts2019-chromium/build_weekly.sh
+++ host/kernel/lts2019-chromium/build_weekly.sh
@@ -5,7 +5,7 @@ mkdir -p host_kernel
 cd host_kernel
 git clone https://github.com/projectceladon/linux-intel-lts2019-chromium.git
 cd linux-intel-lts2019-chromium
-git checkout 1cafe9423ebba269bdee872cb0085f5eecb8b633
+git checkout de8cedc8e73ae5a72b3fe9cb2fa7b7b60aabf118
 cp ../../x86_64_defconfig .config
 patch_list=`find ../../ -iname "*.patch" | sort -u`
 for i in $patch_list
