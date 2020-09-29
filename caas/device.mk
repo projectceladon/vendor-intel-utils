f17846d08c1690d7cd07297fcbe8afec8f6356e1
buildslave <ctbbot@intel.com>
2020-09-29 17:37:27 +0000

    mixin updates
    Revert "Upgrade hwc API version for Android 11"
    This reverts commit 89d151e1ce97b8b07cf499f53851e9a0d42d6dce.
    
    Tracked-On: OAM-93138
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/902

--- caas/device.mk
+++ caas/device.mk
@@ -258,7 +258,8 @@ PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl \
                     android.hardware.graphics.allocator@2.0-impl \
                     android.hardware.graphics.allocator@2.0-service \
                     android.hardware.renderscript@1.0-impl \
-                    android.hardware.graphics.composer@2.3-service\
+                    android.hardware.graphics.composer@2.1-impl \
+                    android.hardware.graphics.composer@2.1-service\
 					android.hardware.health@2.1-service\
 					android.hardware.health@2.1-impl
 
