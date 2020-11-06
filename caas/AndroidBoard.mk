f5419f896c6e165c6246eb06846af247f639e47f
buildslave <ctbbot@intel.com>
2020-11-06 18:40:57 +0000

    mixin updates
    Add perf variants of libraries into product packages
    This enables us to have One-image catering to multiple
    IA CPUs which may or may not support all ISA features.
    Example: Pentium SKUs dont support AVX.
    
    Tracked-On: OAM-94488
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/962
    Signed-off-by: ahs <amrita.h.s@intel.com>

--- caas/AndroidBoard.mk
+++ caas/AndroidBoard.mk
@@ -779,6 +779,37 @@ $(provdata_zip): $(provdata_zip_deps) | $(ACP)
 INSTALLED_RADIOIMAGE_TARGET += $(provdata_zip)
 
 ##############################################################
+# Source: device/intel/mixins/groups/avx/auto/AndroidBoard.mk
+##############################################################
+# kernel modules must be copied before vendorimage is generated
+$(PRODUCT_OUT)/super.img: handle_isa_perf_modules
+
+ISA_PERF_LIB_PATH:= "IA-Perf/avx2"
+isa_perf_libs: avx2_libs
+
+avx2_libs: libneuralnetworks_avx2 \
+	   libaudioprocessing_avx2 \
+	   libRSCpuRef_avx2 \
+	   libart_avx2.com.android.art.debug \
+	   libart_avx2.com.android.art.release \
+	   libartd_avx2.com.android.art.debug
+
+handle_isa_perf_modules: isa_perf_libs
+	mv $(PRODUCT_OUT)/system/lib/$(ISA_PERF_LIB_PATH)/libaudioprocessing_avx2.so $(PRODUCT_OUT)/system/lib/$(ISA_PERF_LIB_PATH)/libaudioprocessing.so
+	mv $(PRODUCT_OUT)/system/lib64/$(ISA_PERF_LIB_PATH)/libaudioprocessing_avx2.so $(PRODUCT_OUT)/system/lib64/$(ISA_PERF_LIB_PATH)/libaudioprocessing.so
+	mv $(PRODUCT_OUT)/system/apex/com.android.neuralnetworks/lib/$(ISA_PERF_LIB_PATH)/libneuralnetworks_avx2.so $(PRODUCT_OUT)/system/apex/com.android.neuralnetworks/lib/$(ISA_PERF_LIB_PATH)/libneuralnetworks.so
+	mv $(PRODUCT_OUT)/system/apex/com.android.neuralnetworks/lib64/$(ISA_PERF_LIB_PATH)/libneuralnetworks_avx2.so $(PRODUCT_OUT)/system/apex/com.android.neuralnetworks/lib64/$(ISA_PERF_LIB_PATH)/libneuralnetworks.so
+	mv $(PRODUCT_OUT)/system/lib/$(ISA_PERF_LIB_PATH)/libRSCpuRef_avx2.so $(PRODUCT_OUT)/system/lib/$(ISA_PERF_LIB_PATH)/libRSCpuRef.so
+	mv $(PRODUCT_OUT)/system/lib64/$(ISA_PERF_LIB_PATH)/libRSCpuRef_avx2.so $(PRODUCT_OUT)/system/lib64/$(ISA_PERF_LIB_PATH)/libRSCpuRef.so
+	mv $(PRODUCT_OUT)/system/apex/com.android.vndk.current/lib/$(ISA_PERF_LIB_PATH)/libRSCpuRef_avx2.so $(PRODUCT_OUT)/system/apex/com.android.vndk.current/lib/$(ISA_PERF_LIB_PATH)/libRSCpuRef.so
+	mv $(PRODUCT_OUT)/system/apex/com.android.vndk.current/lib64/$(ISA_PERF_LIB_PATH)/libRSCpuRef_avx2.so $(PRODUCT_OUT)/system/apex/com.android.vndk.current/lib64/$(ISA_PERF_LIB_PATH)/libRSCpuRef.so
+	mv $(PRODUCT_OUT)/system/apex/com.android.art.release/lib/$(ISA_PERF_LIB_PATH)/libart_avx2.so $(PRODUCT_OUT)/system/apex/com.android.art.release/lib/$(ISA_PERF_LIB_PATH)/libart.so
+	mv $(PRODUCT_OUT)/system/apex/com.android.art.release/lib64/$(ISA_PERF_LIB_PATH)/libart_avx2.so $(PRODUCT_OUT)/system/apex/com.android.art.release/lib64/$(ISA_PERF_LIB_PATH)/libart.so
+	mv $(PRODUCT_OUT)/system/apex/com.android.art.debug/lib/$(ISA_PERF_LIB_PATH)/libart_avx2.so $(PRODUCT_OUT)/system/apex/com.android.art.debug/lib/$(ISA_PERF_LIB_PATH)/libart.so
+	mv $(PRODUCT_OUT)/system/apex/com.android.art.debug/lib64/$(ISA_PERF_LIB_PATH)/libart_avx2.so $(PRODUCT_OUT)/system/apex/com.android.art.debug/lib64/$(ISA_PERF_LIB_PATH)/libart.so
+	mv $(PRODUCT_OUT)/system/apex/com.android.art.debug/lib/$(ISA_PERF_LIB_PATH)/libartd_avx2.so $(PRODUCT_OUT)/system/apex/com.android.art.debug/lib/$(ISA_PERF_LIB_PATH)/libartd.so
+	mv $(PRODUCT_OUT)/system/apex/com.android.art.debug/lib64/$(ISA_PERF_LIB_PATH)/libartd_avx2.so $(PRODUCT_OUT)/system/apex/com.android.art.debug/lib64/$(ISA_PERF_LIB_PATH)/libartd.so
+##############################################################
 # Source: device/intel/mixins/groups/usb-init/true/AndroidBoard.mk
 ##############################################################
 LOAD_MODULES_IN += $(TARGET_DEVICE_DIR)/extra_files/usb-init/load_usb_modules.in
--- caas/device.mk
+++ caas/device.mk
@@ -641,7 +641,14 @@ PRODUCT_PACKAGES += \
 ##############################################################
 # Source: device/intel/mixins/groups/avx/auto/product.mk
 ##############################################################
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/extra_files/avx/checkavx.sh:vendor/bin/checkavx.sh 
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/extra_files/avx/checkavx.sh:vendor/bin/checkavx.sh
+
+PRODUCT_PACKAGES += libaudioprocessing_avx2 \
+		    libneuralnetworks_avx2 \
+		    libRSCpuRef_avx2 \
+		    libart_avx2.com.android.art.debug \
+		    libart_avx2.com.android.art.release \
+		    libartd_avx2.com.android.art.debug
 ##############################################################
 # Source: device/intel/mixins/groups/health/hal/product.mk
 ##############################################################
