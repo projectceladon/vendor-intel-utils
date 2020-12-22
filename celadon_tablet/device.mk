61e5840c1be3995f6ff15818011e177fc205c45e
buildslave <ctbbot@intel.com>
2020-12-22 09:45:22 +0000

    mixin updates
    Change Boot Control HAL to v1.1
    Enable bootctrl 1.1
    
    Tracked-On: OAM-95364
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1025
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/996
    Signed-off-by: JianFeng,Zhou <jianfeng.zhou@intel.com>

--- celadon_tablet/device.mk
+++ celadon_tablet/device.mk
@@ -25,15 +25,11 @@ PRODUCT_PACKAGES += \
     update_engine_client \
     update_verifier \
     libavb \
-    bootctrl.avb \
-    bootctrl.intel \
-    bootctrl.intel.static \
-    bootctrl.intel.recovery \
     update_engine_sideload \
     avbctl \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-service
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.bootctrl=intel
