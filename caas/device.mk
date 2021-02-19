f170a335ecc9edcb1b7f174af32960ecd578f78b
buildslave <ctbbot@intel.com>
2021-02-19 17:23:47 +0000

    mixin updates
    remove vendor.camera-provider-2-4 service from mixin
    vendor.camera-provider-2-4 was not starting properly because
    fail to get required resource
    Solution : USB camera needs android.hardware.camera.provider
    @2.4-external-service and other service of
    vendor.camera-provider-2-4 is removed as its not getting used.
    
    Tracked-On: OAM-95947
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1082
    Signed-off-by: shiva kumara R <shiva.kumara.rudrappa@intel.com>
    Signed-off-by: user189 <shiva.kumara.rudrappa@intel.com>

--- caas/device.mk
+++ caas/device.mk
@@ -265,7 +265,6 @@ PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl \
                     android.hardware.usb@1.0-service \
                     camera.device@1.0-impl \
                     android.hardware.camera.provider@2.4-impl \
-                    android.hardware.camera.provider@2.4-service \
                     android.hardware.graphics.mapper@4.0-impl.minigbm \
                     android.hardware.graphics.allocator@4.0-service.minigbm \
                     android.hardware.renderscript@1.0-impl \
