a4703df0e174de2c1990c227f61406a3d795d3cf
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2021-04-12 10:22:57 +0530

    Update config and diff patches for v5.4.109 rebase
    Tracked-On: OAM-96763
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- host/kernel/lts2019-chromium/build_weekly.sh
+++ host/kernel/lts2019-chromium/build_weekly.sh
@@ -5,7 +5,7 @@ mkdir -p host_kernel
 cd host_kernel
 git clone https://github.com/projectceladon/linux-intel-lts2019-chromium.git
 cd linux-intel-lts2019-chromium
-git checkout 3a80a43b14ca228cefabb5ee198ec53a38886e94
+git checkout 6b51c7539addf8e1558fe4845e8a284179ca1b82
 cp ../../x86_64_defconfig .config
 patch_list=`find ../../ -iname "*.patch" | sort -u`
 for i in $patch_list
