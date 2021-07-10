d248cdeec744404a8a924cc0fef1c26e26655d34
buildslave <ctbbot@intel.com>
2021-07-10 16:35:46 +0000

    mixin updates
    Adding caas_cfc.mk which was missed for
    the caas_cfc target
    
    Tracked-On: OAM-97485
    Mixin-Reviewed-On: https://github.com/projectceladon/device-androidia-mixins/pull/1161
    Signed-off-by: swaroopb <swaroop.balan@intel.com>

--- caas/device.mk
+++ caas/device.mk
@@ -538,15 +538,9 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/extra_files/codecs/media_codecs_performance_cml.xml:vendor/etc/media_codecs_performance.xml
 ##############################################################
-# Source: device/intel/mixins/groups/usb/host+acc/product.mk
+# Source: device/intel/mixins/groups/usb/host/product.mk
 ##############################################################
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:vendor/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:vendor/etc/permissions/android.hardware.usb.host.xml
-
-# usb accessory
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.usb.host.xml:vendor/etc/permissions/android.hardware.usb.host.xml
 
 ##############################################################
 # Source: device/intel/mixins/groups/midi/true/product.mk
