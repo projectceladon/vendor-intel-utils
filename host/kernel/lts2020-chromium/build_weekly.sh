76e7830f4d1d9ef0a16578125f1d703f90e92b82
Lakshmishree C <lakshmishree.c@intel.com>
2022-01-25 14:50:27 +0530

    kernel: Update build script for ref host kernel v5.10.91
    Weekly build script updated with 5.10.91 commit with boot fixes
    
    Tracked-On: OAM-100833
    Signed-off-by: Lakshmishree C <lakshmishree.c@intel.com>

--- host/kernel/lts2020-chromium/build_weekly.sh
+++ host/kernel/lts2020-chromium/build_weekly.sh
@@ -5,7 +5,7 @@ mkdir -p host_kernel
 cd host_kernel
 git clone https://github.com/projectceladon/linux-intel-lts2020-chromium.git
 cd linux-intel-lts2020-chromium
-git checkout 6d47e53f3e106b9fd3be78af0da5a4c3269e4e04
+git checkout 6dda9a5b0c342d16e4cde6fab186a89595de5095
 cp ../../x86_64_defconfig .config
 patch_list=`find ../../ -iname "*.patch" | sort -u`
 for i in $patch_list
