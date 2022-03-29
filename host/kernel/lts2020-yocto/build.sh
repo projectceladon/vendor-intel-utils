ee2d25d9803ca15da0cb031dae70a9df7ffdd103
HeYue <yue.he@intel.com>
2022-03-29 11:35:47 +0800

    Fix the GVT-d boot fail on ADL with hdmi/type-c port
    Tracked-On: OAM-101639
    Signed-off-by: HeYue <yue.he@intel.com>

--- host/kernel/lts2020-yocto/build.sh
+++ host/kernel/lts2020-yocto/build.sh
@@ -9,6 +9,26 @@ cd linux-intel-lts
 git checkout refs/tags/lts-v5.10.100-civ-android-220303T165800Z
 cp ../os.linux.kernel.kernel-config/x86_64_defconfig .config
 
+patch_list=`find ../vendor-intel-utils/host/kernel/lts2020-yocto -iname "*.patch" | sort -u`
+for i in $patch_list
+do
+  a=`grep "Date: " $i`
+  b=`echo ${a#"Date: "}`
+  c=`git log --pretty=format:%aD | grep "$b"`
+  if [[ "$c" == "" ]] ; then
+    git am -3 --keep-cr --whitespace=nowarn $i >& /dev/null
+    if [[ $? == 0 ]]; then
+      echo -e "Patch\t"`basename $i` "\tApplied"
+    else
+      git am --abort
+      echo "Not able to apply\t"`basename $i`
+      exit
+    fi
+  else
+    echo -e "\tAlready applied\t\t"`basename $i`
+  fi
+done
+
 make ARCH=x86_64 clean
 make ARCH=x86_64 olddefconfig
 make ARCH=x86_64 -j64 LOCALVERSION=-yvhb bindeb-pkg
