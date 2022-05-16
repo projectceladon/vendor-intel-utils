5ccb98be720959665e238468490ab6a74655fa33
HeYue <yue.he@intel.com>
2022-05-16 14:28:41 +0800

    Add the lts2020-yocto kernel config file
    Maintain the kernel config in the folder instead of
    cloning the kernel config from innersource.
    
    Tracked-On: OAM-102198
    Signed-off-by: HeYue <yue.he@intel.com>

--- host/kernel/lts2020-yocto/build_weekly.sh
+++ host/kernel/lts2020-yocto/build_weekly.sh
@@ -4,10 +4,9 @@ rm -rf host_kernel
 mkdir -p host_kernel
 cd host_kernel
 git clone https://github.com/intel/linux-intel-lts.git -b 5.10/yocto
-git clone https://github.com/intel-innersource/os.linux.kernel.kernel-config.git -b 5.10/civ_host
 cd linux-intel-lts
 git checkout refs/tags/lts-v5.10.100-civ-android-220303T165800Z
-cp ../os.linux.kernel.kernel-config/x86_64_defconfig .config
+cp ../../x86_64_defconfig .config
 patch_list=`find ../../ -iname "*.patch" | sort -u`
 for i in $patch_list
 do
