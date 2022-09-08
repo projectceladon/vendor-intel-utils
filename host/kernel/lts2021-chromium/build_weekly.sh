c55c611087b5fa1bb1147786458ee72afd255d49
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2022-09-08 13:58:09 +0530

    kernel: Fix patch conflict and build script issues
    With the latest 5.15 kernel + host kernel diff patches, build error is
    seen due to change in function prototype. Also, build script expects
    the user to clone vendor-intel-utils project for it to work.
    
    Fix the build error and also modify the script so one can compile
    by downloading the build script alone.
    
    Tracked-On: OAM-103824
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- host/kernel/lts2021-chromium/build_weekly.sh
+++ host/kernel/lts2021-chromium/build_weekly.sh
@@ -3,11 +3,15 @@
 rm -rf host_kernel
 mkdir -p host_kernel
 cd host_kernel
+git clone https://github.com/projectceladon/vendor-intel-utils.git
+cd vendor-intel-utils
+git checkout 7507c2fa8b6b548e3889b4c5fc32d5511a8af07e
+cd ../
 git clone https://github.com/projectceladon/linux-intel-lts2021-chromium.git
 cd linux-intel-lts2021-chromium
-git checkout 3c7852fc6d88bfff414f790113249b12030f30ce
-cp ../../x86_64_defconfig .config
-patch_list=`find ../../ -iname "*.patch" | sort -u`
+git checkout 94ca6268827aa5389b6e560dbed0b79f537b8f59
+cp ../vendor-intel-utils/host/kernel/lts2021-chromium/x86_64_defconfig .config
+patch_list=`find  ../vendor-intel-utils/host/kernel/lts2021-chromium -iname "*.patch" | sort -u`
 for i in $patch_list
 do
   a=`grep "Date: " $i`
