524f0e8fa3ad099b57344c42ba10a2f225898273
rajucm <raju.mallikarjun.chegaraddi@intel.com>
2022-11-08 00:13:35 +0530

    Kernel rebase v5.15.74 on master branch 2022-11-08
    rebasing kernel version from 5.15.44 to 5.15.74
    also, updating build_weekly.sh with latest kernel version-tag
    
    Tracked-On: OAM-104057
    Signed-off-by: rajucm <raju.mallikarjun.chegaraddi@intel.com>

--- host/kernel/lts2021-chromium/build_weekly.sh
+++ host/kernel/lts2021-chromium/build_weekly.sh
@@ -3,20 +3,24 @@
 rm -rf host_kernel
 mkdir -p host_kernel
 cd host_kernel
+
 git clone https://github.com/projectceladon/vendor-intel-utils.git
 cd vendor-intel-utils
 git checkout 7d246edc3370d823194d2306ca2a484568dea75e
+
 cd ../
-git clone https://github.com/projectceladon/linux-intel-lts2021-chromium.git
+tag=lts-v5.15.74-20221108-r1
+git clone -b $tag https://github.com/projectceladon/linux-intel-lts2021-chromium.git
 cd linux-intel-lts2021-chromium
-git checkout 94ca6268827aa5389b6e560dbed0b79f537b8f59
+
 cp ../vendor-intel-utils/host/kernel/lts2021-chromium/x86_64_defconfig .config
 patch_list=`find  ../vendor-intel-utils/host/kernel/lts2021-chromium -iname "*.patch" | sort -u`
+
 for i in $patch_list
 do
   a=`grep "Date: " $i`
   b=`echo ${a#"Date: "}`
-  c=`git log --pretty=format:%aD | grep "$b"`
+  c=`git log --pretty=format:%aD -100| grep "$b"`
   if [[ "$c" == "" ]] ; then
     git am -3 --keep-cr --whitespace=nowarn $i >& /dev/null
     if [[ $? == 0 ]]; then
@@ -30,6 +34,7 @@ do
     echo -e "\tAlready applied\t\t"`basename $i`
   fi
 done
+
 make ARCH=x86_64 clean
 make ARCH=x86_64 olddefconfig
 make ARCH=x86_64 -j64 LOCALVERSION=-cvhb bindeb-pkg
