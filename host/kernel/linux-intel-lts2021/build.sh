23c6ffb20de0e7bc3674b15efe2b9befb886fe13
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2022-12-12 08:48:17 +0530

    Update host kernel diff version update to fix sriov boot issue
    Host kernel and diff patch commit hash/tag update to point to
    sriov boot issue fix "Fix guest not getting started in SRIOV mode"
    
    Tracked-On: OAM-105229
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- host/kernel/linux-intel-lts2021/build.sh
+++ host/kernel/linux-intel-lts2021/build.sh
@@ -5,9 +5,6 @@ mkdir -p host_kernel
 cd host_kernel
 
 git clone https://github.com/projectceladon/vendor-intel-utils.git
-cd vendor-intel-utils
-git checkout 240d89c245b43f3d1e24af39a8b467200d75411e
-cd ../
 
 git clone -b main https://github.com/projectceladon/linux-intel-lts2021.git
 cd linux-intel-lts2021
