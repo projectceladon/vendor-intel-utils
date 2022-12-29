6992aea47414cab735fc552376bddf95b410d29d
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2022-12-29 12:16:50 +0000

    ics3a: configurability for test_irrv script
    Issue: VSMGWL-51998
    
    Added below changes to facilitate addition of more tests:
    - Modified test_irrv.sh script to allow caller to specify different resolutions
    - Script now allows caller to opt for a smaller "basic" test set
    - Updated test.yml to align with above script changes
    - Minor change for test summary report

--- scripts/test_irrv.sh
+++ scripts/test_irrv.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 # Usage:
-#  test_irrv.sh /path/to/aic/workdir streaming:latest icr:latest
+#  test_irrv.sh /path/to/aic/workdir streaming:latest icr:latest Profile.yml WidthxHeight [test-set]
+#
+# -- Profile.yml is expected to be in /path/to/aic/workdir
+# -- test-set is optional. Supported options: "all" (default), "basic".
+#    - Any input other than supported ones above will use the default option
 
 SCRIPT_PATH=$(dirname $(which $0))
 
 AIC_WORKDIR=$(realpath $1)
 ESC=$2
 ICR=$3
+PROFILE=$4
+RESOLUTION=$5
+TEST_SET=${6:-all}
+
+QUALITY=4
+TARGET_BITRATE=3.3M
+MAX_BITRATE=6.6M
 
 set -x
 
@@ -28,7 +39,7 @@ AIC_DOCKER="docker create -u $(id -u):$(id -g) \
   --network host -v $AIC_WORKDIR:/opt/workdir \
   --name emu0 \
   $ICR \
-    aic-emu --cmd /opt/workdir/720p.yml \
+    aic-emu --cmd /opt/workdir/$PROFILE \
       --content /opt/workdir/test.bgra \
       --hwc-sock /opt/workdir/hwc-sock \
       --device $DEVICE"
@@ -51,25 +62,35 @@ VAINFO_DOCKER="docker run --rm \
   --name vainfo0 \
   $ICR"
 
-ffmpeg_cmd="ffmpeg -f lavfi -i testsrc=size=1280x720:rate=30 -pix_fmt bgra -frames:v 150 -f rawvideo -y /opt/workdir/test.bgra"
+ffmpeg_cmd="ffmpeg -f lavfi -i testsrc=size=${RESOLUTION}:rate=30 -pix_fmt bgra -frames:v 150 -f rawvideo -y /opt/workdir/test.bgra"
 
-icr_cases=(
+icr_cases_all=(
   # h264
-  "icr_encoder 0 -streaming -res 1280x720 -fr 30 -url irrv:264 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res 1280x720 -fr 30 -url irrv:264 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res 1280x720 -fr 60 -url irrv:264 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res 1280x720 -fr 60 -url irrv:264 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
   # h265
-  "icr_encoder 0 -streaming -res 1280x720 -fr 30 -url irrv:265 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res 1280x720 -fr 30 -url irrv:265 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res 1280x720 -fr 60 -url irrv:265 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res 1280x720 -fr 60 -url irrv:265 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
   # av1
-  "icr_encoder 0 -streaming -res 1280x720 -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res 1280x720 -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res 1280x720 -fr 60 -url irrv:av1 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
-  "icr_encoder 0 -streaming -res 1280x720 -fr 60 -url irrv:av1 -plugin qsv -lowpower -quality 4 -ratectrl VBR -b 3.3M -maxrate 6.6M -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
-  )
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+)
+
+icr_cases_basic=(
+  # Basic set covering Codec, FrameRate and TCAE settings
+  # h264
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:264 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 1 -hwc_sock /opt/workdir/hwc-sock"
+  # h265
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 60 -url irrv:265 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+  # av1
+  "icr_encoder 0 -streaming -res ${RESOLUTION} -fr 30 -url irrv:av1 -plugin qsv -lowpower -quality ${QUALITY} -ratectrl VBR -b ${TARGET_BITRATE} -maxrate ${MAX_BITRATE} -tcae 0 -hwc_sock /opt/workdir/hwc-sock"
+)
 
 function get_framerate() {
   echo "$1" | sed -E 's/.*fr ([0-9\.]*).*/\1/'
@@ -91,12 +112,21 @@ function check_av1() {
 
 $FFMPEG_DOCKER $ffmpeg_cmd
 
+icr_cases=("${icr_cases_all[@]}")
+if [ "${TEST_SET}" == "basic" ]
+then
+    icr_cases=("${icr_cases_basic[@]}")
+fi
+
 count=0
+success=0
 
 for c in "${icr_cases[@]}"; do
   echo ">>> Testing with:"
   echo "$c"
   echo "<<<"
+
+  fail=0
 
   if is_av1_test "$c" && ! check_av1; then
     echo "Skipping test since av1 encoding is not supported"
@@ -104,23 +134,31 @@ for c in "${icr_cases[@]}"; do
   fi
 
   if ! $AIC_DOCKER; then
-    res=1
+    fail=1
   fi
   if ! $ICR_DOCKER $c; then
-    res=1
+    fail=1
   fi
   docker start emu0
   docker start icr0
   if ! $ESC_DOCKER irrv-client-test --framerate $(get_framerate "$c"); then
-    res=1
+    fail=1
   fi
-  docker logs icr0 > $AIC_WORKDIR/logs_$count.icr0.txt 2>&1
-  docker logs emu0 > $AIC_WORKDIR/logs_$count.emu0.txt 2>&1
+  docker logs icr0 > $AIC_WORKDIR/logs_$count.${PROFILE%.*}.icr0.txt 2>&1
+  docker logs emu0 > $AIC_WORKDIR/logs_$count.${PROFILE%.*}.emu0.txt 2>&1
   docker stop icr0 emu0
   docker rm icr0 emu0
 
+  if [[ $fail -eq 0 ]]; then
+      success=$((++success))
+  else
+      res=1
+  fi
+
   count=$((++count))
 done
+
+echo $PROFILE: $success tests passed \($count run\)
 
 exit $res
 
