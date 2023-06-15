32a10d08aeb9bc65caf99f0f68e0e9dafb8ad146
Boppana, Tej Kiran <tej.kiran.boppana@intel.com>
2023-06-15 12:00:15 +0530

    CVE: Update build_weekly.sh with latest commit tag
    BDSA-2022-4147(CVE-2023-2124) - Fix Applied
    BDSA-2023-1049(CVE-2023-31436) - Fix Applied
    BDSA-2023-1125(CVE-2023-2156) - Fix Applied
    BDSA-2022-4083(CVE-2023-2166) - Fix Applied
    BDSA-2023-1114(CVE-2023-2513) - Fix Applied
    BDSA-2023-0999(CVE-2023-2269) - Fix Applied
    BDSA-2022-4084(CVE-2023-2177) - Fix Applied
    CVE-2023-33203 - Fix Applied
    CVE-2023-0045 - Fix Applied
    
    Tracked-On: OAM-109987
    Signed-off-by: Boppana, Tej Kiran <tej.kiran.boppana@intel.com>

--- host/kernel/lts2020-chromium/build_weekly.sh
+++ host/kernel/lts2020-chromium/build_weekly.sh
@@ -4,7 +4,7 @@ rm -rf host_kernel
 mkdir -p host_kernel
 cd host_kernel
 
-tag="lts-v5.10.118-20230426-r10"
+tag="lts-v5.10.118-20230615-r11"
 git clone -b $tag https://github.com/projectceladon/linux-intel-lts2020-chromium.git
 cd linux-intel-lts2020-chromium
 
