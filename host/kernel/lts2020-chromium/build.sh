a754220dcd18469ee08c99de78507c24a62241b4
rajucm <raju.mallikarjun.chegaraddi@intel.com>
2023-07-13 13:17:28 +0000

    kernel: update build_weekly.sh for lts2020-chromium base
    update latest tag="lts-v5.10.118-20230713-r12 into
    build_weekly.sh
    
    Tracked-On: OAM-111233
    Signed-off-by: rajucm <raju.mallikarjun.chegaraddi@intel.com>

--- host/kernel/lts2020-chromium/build.sh
+++ host/kernel/lts2020-chromium/build.sh
@@ -3,18 +3,19 @@
 rm -rf host_kernel
 mkdir -p host_kernel
 cd host_kernel
-git clone https://github.com/projectceladon/vendor-intel-utils
+branch_name="celadon/s/mr0/master"
 
-branch_name="celadon/s/mr0/stable"
+git clone -b $branch_name https://github.com/projectceladon/vendor-intel-utils
 git clone -b $branch_name https://github.com/projectceladon/linux-intel-lts2020-chromium.git
 cd linux-intel-lts2020-chromium
+
 cp ../vendor-intel-utils/host/kernel/lts2020-chromium/x86_64_defconfig .config
 patch_list=`find ../vendor-intel-utils/host/kernel/lts2020-chromium -iname "*.patch" | sort -u`
 for i in $patch_list
 do
   a=`grep "Date: " $i`
   b=`echo ${a#"Date: "}`
-  c=`git log --pretty=format:%aD | grep "$b"`
+  c=`git log --pretty=format:%aD -1000 | grep "$b"`
   if [[ "$c" == "" ]] ; then
     git am -3 --keep-cr --whitespace=nowarn $i >& /dev/null
     if [[ $? == 0 ]]; then
@@ -28,6 +29,7 @@ do
     echo -e "\tAlready applied\t\t"`basename $i`
   fi
 done
+
 make ARCH=x86_64 clean
 make ARCH=x86_64 olddefconfig
 make ARCH=x86_64 -j64 LOCALVERSION=-cvhb bindeb-pkg
