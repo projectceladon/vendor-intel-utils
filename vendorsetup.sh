01b6ab91986bacb04720a4e0908307c96d887b69
swaroopb <swaroop.balan@intel.com>
2024-06-25 09:32:13 +0530

    Revert "New extended lunch target for base_aaos"
    This reverts commit 0a2209698a02c60dd26db394b7017106271cb283.
    
    Tracked-On: OAM-121246
    Signed-off-by: swaroopb <swaroop.balan@intel.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -30,18 +30,14 @@ function lunch
     if [[ $(find device/intel -path "*mixins*" -prune -o -name ${TARGET_PRODUCT}.mk -print) ]]; then
         product_dir=$(dirname $(find device/intel -path "*mixins*" -prune -o -name ${TARGET_PRODUCT}.mk -print))
 	echo "Executing mixin update..."
-	if [[ ${TARGET_PRODUCT} == "aaos_iasw" ]]; then
-          mixinup -s device/intel/project-celadon/base_aaos/mixins.spec
-	else
-          mixinup -s $product_dir/mixins.spec
-	fi
+        mixinup -s $product_dir/mixins.spec
     fi
 }
 
 # Get the exact value of a build variable.
 function get_build_var()
 {
-    if [ "$1" = "COMMON_LUNCH_CHOICESS" ]
+    if [ "$1" = "COMMON_LUNCH_CHOICES" ]
     then
         valid_targets=`mixinup -t`
         save=`build/soong/soong_ui.bash --dumpvar-mode $1`
